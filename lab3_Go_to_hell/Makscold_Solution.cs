465f2d318469870b172ff770dc8949d1700225b8
Makscoldster <Makscoldster773@gmail.com>
2025-03-14 21:51:01 +0200

    Добавьте файлы проекта.

---
+++ lab3_Go_to_hell/Makscold_Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public static class MakscoldSolution
+    {
+        public static void Foo(int[][] stack)
+        {
+            Console.WriteLine("Makscold Solution");
+            foreach (var arr in stack)
+            {
+                Console.WriteLine(String.Join(' ', arr));
+            }
+        }
+    }
+}
---
+++ lab3_Go_to_hell/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace lab3_Go_to_hell
+{
+    iinternal class Program
+    {
+        static int[][] Input()
+        {
+            Console.WriteLine("Введіть кількість масивів");
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] stack = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введіть елементи масиву номер {i + 1} через пробіл");
+                stack[i] = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            }
+            return stack;
+        }
+        static byte Choice()
+        {
+            Console.WriteLine("Виберіть умову завдання:\n" +
+                              "1) \n" +
+                              "2) \n" +
+                              "3) \n" +
+                              "0) Вийти з програми");
+
+            byte choice;
+            byte countOfBlocks = 4;
+            do
+            {
+                try
+                {
+                    choice = byte.Parse(Console.ReadLine());
+                    if (choice <= countOfBlocks) return choice;
+                    Problem();
+                }
+                catch { Problem(); }
+            } while (true);
+        }
+        static void Problem() => Console.WriteLine("Спробуйте ще раз");
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            do
+            {
+                //bool create = true;
+
+                byte choice = Choice();
+                if (choice == 0) return;
+
+                int[][] jagged = Input();
+
+                Action action = choice switch
+                {
+                    1 => () => MakscoldSolution.Foo(jagged),
+                    2 => () => Problem(),
+                    3 => () => Problem(),
+                    _ => () => Problem(),
+                };
+                action();
+
+                //Console.WriteLine("Ввести нову матрицю?\n" +
+                //                  "1) Так\n" +
+                //                  "Other) Ні");
+
+
+
+            } while (true);
+
+        }
+    }
+}
