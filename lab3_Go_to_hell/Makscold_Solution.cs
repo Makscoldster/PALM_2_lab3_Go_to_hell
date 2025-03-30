b4181dff15c382e545357e5fa991cfa13f07e14d
Mariia Guk <mashahuk27@gmail.com>
2025-03-30 23:02:14 +0300

    First Global Fix Commit

--- lab3_Go_to_hell/Makscold_Solution.cs
+++ lab3_Go_to_hell/Makscold_Solution.cs
@@ -10,18 +10,18 @@ namespace Lab3
 {
     public static class MakscoldSolution
     {//15. Вставити після кожного парного елемента елемент із значенням 0
-        public static void ZeroAfterEven(int[][] jagged)
+        public static void TestZeroAfterEven(ref int[][] jagged)
         {
             Console.WriteLine("Початковий масив:");
-            Program.PrintJagged(jagged);
+            JaggedArray.PrintJagged(jagged);
 
-            int[] countOfEven = CountOfEven(jagged);
+            int[] countOfEven = TestCountOfEven(jagged);
 
             int[][] result = new int[jagged.Length][];
 
             for (int i = 0; i < result.Length; i++)
             {
-                result[i] = new int[jagged[i].Length+countOfEven[i]]; // Initialize the inner array
+                result[i] = new int[jagged[i].Length + countOfEven[i]]; // Initialize the inner array
                 int indexOfJagged = 0;
                 for (int j = 0; j < result[i].Length; j++)
                 {
@@ -32,9 +32,9 @@ namespace Lab3
             jagged = result;
 
             Console.WriteLine("Кінцевий масив:");
-            Program.PrintJagged(jagged);
+            JaggedArray.PrintJagged(jagged);
         }
-        static int[] CountOfEven(int[][] jagged)
+        static int[] TestCountOfEven(int[][] jagged)
         {
             int[] countOfEven = new int[jagged.Length];
             for (int i = 0; i < jagged.Length; i++)
@@ -47,6 +47,38 @@ namespace Lab3
                 countOfEven[i] = count;
             }
             return countOfEven;
+        }
+
+        //15. Вставити після кожного парного елемента елемент із значенням 0
+        public static void ZeroAfterEven(ref int[] array)
+        {
+            Console.WriteLine("Початковий масив:");
+            OneDimensionalArray.PrintArray(array);
+
+            int countOfEven = CountOfEven(array);
+
+            int[] result = new int[array.Length + countOfEven];
+
+
+            int indexOfarray = 0;
+            for (int j = 0; j < result.Length; j++)
+            {
+                result[j] = array[indexOfarray++];
+                if (result[j] % 2 == 0 && j + 1 < result.Length) result[++j] = 0;
+            }
+            array = result;
+
+            Console.WriteLine("Кінцевий масив:");
+            OneDimensionalArray.PrintArray(array);
+        }
+        static int CountOfEven(int[] array)
+        {
+            int count = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] % 2 == 0) count++;
+            }
+            return count;
         }
     }
 }
